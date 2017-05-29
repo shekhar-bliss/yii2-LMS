3b802eb4ee9682274cf5bf6db8648b40739f1a7a
Shekhar <shekhar.blisstering@gmail.com>
2017-05-29 06:47:40 +0530

    Override user registration form

--- common/config/main.php
+++ common/config/main.php
@@ -5,10 +5,32 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@dektrium/user/views' => '@app/views/user'
+                ],
+            ],
+        ],
+        'mail' => [
+           'class' => 'yii\swiftmailer\Mailer',
+           'transport' => [
+               'class' => 'Swift_SmtpTransport',
+               'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+               'username' => 'shekhar.blisstering@gmail.com',
+               'password' => 'Shekhar123$',
+               'port' => '587', // Port 25 is a very common port too
+               'encryption' => 'tls', // It is often used, check your provider or mail server specs
+           ],
+       ],
     ],
     'modules' => [
 		    'user' => [
 		        'class' => 'dektrium\user\Module',
+                'modelMap' => [
+                    'RegistrationForm' => 'app\models\RegistrationForm',
+                    'User' => 'app\models\User',
+                ]
 		    ],
 		],
 ];
