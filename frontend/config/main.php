ef0ec804d4453a124e4215a052943f34a937e19d
Shekhar <shekhar.blisstering@gmail.com>
2017-05-29 08:16:10 +0530

    Define application name

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name'=>'Vision Study',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
